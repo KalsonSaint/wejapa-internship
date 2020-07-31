96bec38c67be62d59145074a4e035dd6635ed9bd
KalsonSaint <saintkalson@gmail.com>
2020-07-31 15:49:59 +0100

    Updates

---
+++ precache-manifest.61f4e3f0d0297cf992304f8d81136450.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb48130a643f804196fce2fa70d8fd09",
+    "url": "/wejapa-internship/index.html"
+  },
+  {
+    "revision": "9f11b6fc27151cb742c0",
+    "url": "/wejapa-internship/static/css/main.bbfedd2d.chunk.css"
+  },
+  {
+    "revision": "8978f7075d568f51065d",
+    "url": "/wejapa-internship/static/js/2.02ebb880.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wejapa-internship/static/js/2.02ebb880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f11b6fc27151cb742c0",
+    "url": "/wejapa-internship/static/js/main.a46bf084.chunk.js"
+  },
+  {
+    "revision": "e680fed066a0457fc34c",
+    "url": "/wejapa-internship/static/js/runtime-main.ec660b76.js"
+  }
+]);
