4cfc0f12f890e815dfab8327e3e1dbbea4bf5e67
KalsonSaint <saintkalson@gmail.com>
2020-07-31 13:15:59 +0100

    Search Box Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{Component}  from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -7,17 +7,28 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hello Kalson'
+      monsters: [],
+      searchField: ''
     }
   }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return(
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({string: 'Welcome Kalson'})}>Change Text</button>
-        </header>
+        <input type="search" placeholder="Search Monsters" 
+          onChange={e => this.setState({ searchField: e.target.value})}/>
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
